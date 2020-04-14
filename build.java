39f2963c7e890839ab7c8dabfea1bd1cf378a2a7
yangyangyou <63649925+yangyangyou@users.noreply.github.com>
2020-04-14 22:14:05 +0800

    Create build.java

---
+++ build.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.hongqiao_2.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.hongqiao_2.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "4.2.0";
+}
